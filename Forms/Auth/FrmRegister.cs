f24644ab58b20e54d2679b00b7a110eea12eb960
Kevin <briankevinbotta@hotmail.com>
2025-09-23 07:33:34 -0300

    cambios register

--- Forms/Auth/FrmRegister.cs
+++ Forms/Auth/FrmRegister.cs
@@ -18,36 +18,39 @@ namespace MicheBytesRecipes.Forms.Auth
             InitializeComponent();
 
             //Setea los cues en los textbox
-            CueProvider.SetCue(TxtNombre, "Ingresa tu nombre");
-            CueProvider.SetCue(TxtApellido, "Ingresa tu apellido");
-            CueProvider.SetCue(TxtTelefono, "Ingresa tu teléfono");
-            CueProvider.SetCue(TxtEmail, "Ingresa tu correo electrónico");
-            CueProvider.SetCue(TxtContra, "Ingresa tu contraseña");
-            CueProvider.SetCue(TxtRepContra, "Repeti tu contraseña");
-            CueProvider.SetCue(TxtRecupero, "Ingresa una palabra de recuperacion");
+            CueProvider.SetCue(txtNombre, "Ingresa tu nombre");
+            CueProvider.SetCue(txtApellido, "Ingresa tu apellido");
+            CueProvider.SetCue(txtTelefono, "Ingresa tu teléfono");
+            CueProvider.SetCue(txtEmail, "Ingresa tu correo electrónico");
+            CueProvider.SetCue(txtContra, "Ingresa tu contraseña");
+            CueProvider.SetCue(txtRepContra, "Repeti tu contraseña");
 
             //Redondeo de botones, paneles y textbox
-            UiHelpers.SetRoundedTextBox(TxtNombre, 20);
-            UiHelpers.SetRoundedTextBox(TxtApellido, 20);
-            UiHelpers.SetRoundedTextBox(TxtTelefono, 20);
-            UiHelpers.SetRoundedTextBox(TxtEmail, 20);
-            UiHelpers.SetRoundedTextBox(TxtContra, 20);
-            UiHelpers.SetRoundedTextBox(TxtRepContra, 20);
-            UiHelpers.SetRoundedTextBox(TxtRecupero, 20);
-            UiHelpers.SetRoundedButton(BtnRegistrar, 20);
+            /*UiHelpers.SetRoundedTextBox(txtNombre, 20);
+            UiHelpers.SetRoundedTextBox(txtApellido, 20);
+            UiHelpers.SetRoundedTextBox(txtTelefono, 20);
+            UiHelpers.SetRoundedTextBox(txtEmail, 20);
+            UiHelpers.SetRoundedTextBox(txtContra, 20);
+            UiHelpers.SetRoundedTextBox(txtRepContra, 20);*/
+            UiHelpers.SetRoundedButton(btnRegistrar, 20);
 
             //Aplicación del tema y color gradiente al formulario y panel derecho
             ThemeManager.ApplyTheme(this);
-            UiHelpers.SetGradient(this, Color.FromArgb(0, 10, 20), Color.FromArgb(10, 30, 50), System.Drawing.Drawing2D.LinearGradientMode.Vertical);
-            UiHelpers.SetGradient(PanelRight, Color.FromArgb(0, 10, 20), Color.FromArgb(10, 30, 50), System.Drawing.Drawing2D.LinearGradientMode.Vertical);
+            UiHelpers.SetGradient(pnlRight, Color.FromArgb(0, 10, 20), Color.FromArgb(10, 30, 50), System.Drawing.Drawing2D.LinearGradientMode.Vertical);
+            UiHelpers.SetGradient(pnlRight, Color.FromArgb(0, 10, 20), Color.FromArgb(10, 30, 50), System.Drawing.Drawing2D.LinearGradientMode.Vertical);
+            ThemeManager.ApplyTheme(this);
+            UiHelpers.SetGradient(pnlLeft, Color.FromArgb(0, 10, 20), Color.FromArgb(10, 30, 50), System.Drawing.Drawing2D.LinearGradientMode.Vertical);
+            UiHelpers.SetGradient(pnlLeft, Color.FromArgb(0, 10, 20), Color.FromArgb(10, 30, 50), System.Drawing.Drawing2D.LinearGradientMode.Vertical);
+            txtContra.UseSystemPasswordChar = true;
+            txtRepContra.UseSystemPasswordChar = true;
 
         }
 
-        private void PanelRight_Paint(object sender, PaintEventArgs e)
+        private void pnlRight_Paint(object sender, PaintEventArgs e)
         {
 
             //Hacer transparentes los labels y checkbox del panel derecho. Pero no afecta el setcue
-            foreach (Control control in PanelRight.Controls)
+            foreach (Control control in pnlRight.Controls)
             {
                 if (control is Label)
                 {
@@ -55,8 +58,22 @@ namespace MicheBytesRecipes.Forms.Auth
                 }
             }
 
-            LinkTerminos.BackColor = Color.Transparent;
+            lblLinkTerminos.BackColor = Color.Transparent;
             LinkIniciar.BackColor = Color.Transparent;
+
+        }
+        private void pnlLeft_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Control control in pnlRight.Controls)
+            {
+                if (control is Label)
+                {
+                    control.BackColor = Color.Transparent;
+                }
+            }
+            lblTituloLeft.BackColor = Color.Transparent;
+            lblRelleno.BackColor = Color.Transparent;
+            lblTexto.BackColor = Color.Transparent;
 
         }
 
@@ -88,5 +105,39 @@ namespace MicheBytesRecipes.Forms.Auth
                 pbxFotoPerfil.Image = Image.FromFile(ofdFotoPerfil.FileName);
             }
         }
+
+        private void btnViewContra_MouseDown(object sender, MouseEventArgs e)
+        {
+            txtContra.UseSystemPasswordChar = false;
+            txtContra.PasswordChar = '\0';
+
+        }
+
+        private void btnViewContra_MouseUp(object sender, MouseEventArgs e)
+        {
+            txtContra.UseSystemPasswordChar = true;
+            txtContra.PasswordChar = '●';
+            
+        }
+
+        private void btnViewAgain_MouseDown(object sender, MouseEventArgs e)
+        {
+            txtRepContra.UseSystemPasswordChar = false;
+            txtRepContra.PasswordChar = '\0';
+
+        }
+
+        private void btnViewAgain_MouseUp(object sender, MouseEventArgs e)
+        {
+            txtRepContra.UseSystemPasswordChar = true;
+            txtRepContra.PasswordChar = '●';
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            txtNombre.Text = txtContra.Text;
+        }
+
+
     }
 }
