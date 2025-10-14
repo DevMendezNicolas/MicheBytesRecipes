3e0d897c79b942455bc7a9ca0c85d8d4f539e5c8
Nicolas <dev.mendez15@gmail.com>
2025-10-14 00:58:52 -0300

    asf

--- Managers/GestorReceta.cs
+++ Managers/GestorReceta.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Drawing;
 using System.Windows.Forms;
+using MicheBytesRecipes.Classes;
 using MicheBytesRecipes.Classes.Recetas;
 using MicheBytesRecipes.Connections;
 using MicheBytesRecipes.Helpers;
