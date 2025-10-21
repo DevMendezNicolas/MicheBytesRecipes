d4c3c606c3d79034748075df8491084c9e65c2e0
Kevin <briankevinbotta@hotmail.com>
2025-10-21 02:15:01 -0300

    asd

--- Forms/User/UcRecetaTarjeta.Designer.cs
+++ Forms/User/UcRecetaTarjeta.Designer.cs
@@ -43,59 +43,60 @@
             // 
             // lblNombreReceta
             // 
-            this.lblNombreReceta.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            this.lblNombreReceta.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.lblNombreReceta.AutoSize = true;
-            this.lblNombreReceta.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNombreReceta.Location = new System.Drawing.Point(3, 10);
+            this.lblNombreReceta.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreReceta.Location = new System.Drawing.Point(7, 9);
             this.lblNombreReceta.Name = "lblNombreReceta";
-            this.lblNombreReceta.Size = new System.Drawing.Size(149, 25);
+            this.lblNombreReceta.Size = new System.Drawing.Size(160, 33);
             this.lblNombreReceta.TabIndex = 1;
             this.lblNombreReceta.Text = "Nombre Receta";
+            this.lblNombreReceta.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lblCategoria
             // 
             this.lblCategoria.AutoSize = true;
-            this.lblCategoria.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblCategoria.Location = new System.Drawing.Point(86, 149);
+            this.lblCategoria.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCategoria.Location = new System.Drawing.Point(79, 146);
             this.lblCategoria.Name = "lblCategoria";
-            this.lblCategoria.Size = new System.Drawing.Size(77, 21);
+            this.lblCategoria.Size = new System.Drawing.Size(65, 17);
             this.lblCategoria.TabIndex = 2;
             this.lblCategoria.Text = "Categoria";
             // 
             // lblPais
             // 
             this.lblPais.AutoSize = true;
-            this.lblPais.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPais.Location = new System.Drawing.Point(86, 173);
+            this.lblPais.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPais.Location = new System.Drawing.Point(79, 170);
             this.lblPais.Name = "lblPais";
-            this.lblPais.Size = new System.Drawing.Size(58, 21);
+            this.lblPais.Size = new System.Drawing.Size(48, 17);
             this.lblPais.TabIndex = 3;
             this.lblPais.Text = "Origen";
             // 
             // lblDificultad
             // 
             this.lblDificultad.AutoSize = true;
-            this.lblDificultad.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDificultad.Location = new System.Drawing.Point(86, 197);
+            this.lblDificultad.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDificultad.Location = new System.Drawing.Point(79, 194);
             this.lblDificultad.Name = "lblDificultad";
-            this.lblDificultad.Size = new System.Drawing.Size(76, 21);
+            this.lblDificultad.Size = new System.Drawing.Size(62, 17);
             this.lblDificultad.TabIndex = 4;
             this.lblDificultad.Text = "Dificultad";
             // 
             // lblTiempo
             // 
             this.lblTiempo.AutoSize = true;
-            this.lblTiempo.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTiempo.Location = new System.Drawing.Point(86, 220);
+            this.lblTiempo.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTiempo.Location = new System.Drawing.Point(79, 217);
             this.lblTiempo.Name = "lblTiempo";
-            this.lblTiempo.Size = new System.Drawing.Size(62, 21);
+            this.lblTiempo.Size = new System.Drawing.Size(52, 17);
             this.lblTiempo.TabIndex = 5;
             this.lblTiempo.Text = "Tiempo";
             // 
             // pbImagenReceta
             // 
-            this.pbImagenReceta.Location = new System.Drawing.Point(24, 63);
+            this.pbImagenReceta.Location = new System.Drawing.Point(16, 57);
             this.pbImagenReceta.Name = "pbImagenReceta";
             this.pbImagenReceta.Size = new System.Drawing.Size(128, 74);
             this.pbImagenReceta.TabIndex = 0;
@@ -104,40 +105,40 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 149);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 144);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(80, 21);
+            this.label1.Size = new System.Drawing.Size(77, 20);
             this.label1.TabIndex = 6;
             this.label1.Text = "Categoria:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 173);
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 168);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 21);
+            this.label2.Size = new System.Drawing.Size(57, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "Origen:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(4, 197);
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 192);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(79, 21);
+            this.label3.Size = new System.Drawing.Size(77, 20);
             this.label3.TabIndex = 4;
             this.label3.Text = "Dificultad:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(4, 220);
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(4, 215);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(65, 21);
+            this.label4.Size = new System.Drawing.Size(63, 20);
             this.label4.TabIndex = 5;
             this.label4.Text = "Tiempo:";
             // 
@@ -157,7 +158,7 @@
             this.Controls.Add(this.lblNombreReceta);
             this.Controls.Add(this.pbImagenReceta);
             this.Name = "UcRecetaTarjeta";
-            this.Size = new System.Drawing.Size(192, 258);
+            this.Size = new System.Drawing.Size(170, 240);
             this.Click += new System.EventHandler(this.UcRecetaTarjeta_Click);
             ((System.ComponentModel.ISupportInitialize)(this.pbImagenReceta)).EndInit();
             this.ResumeLayout(false);
