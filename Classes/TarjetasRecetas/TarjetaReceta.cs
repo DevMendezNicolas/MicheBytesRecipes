f2c6d40fbf5654f916ba7678fa0f2e3bb07b5126
Kevin <briankevinbotta@hotmail.com>
2025-11-03 00:47:09 -0300

    User control imagenes default

--- Classes/TarjetasRecetas/TarjetaReceta.cs
+++ Classes/TarjetasRecetas/TarjetaReceta.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
 
 namespace MicheBytesRecipes.Classes.TarjetasRecetas
 {
@@ -19,7 +21,7 @@ namespace MicheBytesRecipes.Classes.TarjetasRecetas
 
         public TarjetaReceta() { }
 
-        // Constructor desde Receta
+        // Constructor desde tu entidad Receta
         public TarjetaReceta(Receta receta, GestorCatalogo catalogo)
         {
             RecetaId = receta.RecetaId;
@@ -28,7 +30,10 @@ namespace MicheBytesRecipes.Classes.TarjetasRecetas
             PaisReceta = catalogo.ObtenerPaisPorId(receta.PaisId)?.Nombre ?? "Desconocido";
             TiempoReceta = receta.TiempoPreparacion.ToString(@"hh\:mm");
             DificultadReceta = receta.NivelDificultad.ToString();
+
+            // Asignar directamente la imagen de la receta (puede ser null)
             ImagenReceta = receta.ImagenReceta;
         }
+
     }
 }
